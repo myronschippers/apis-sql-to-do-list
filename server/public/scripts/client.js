09b21a10da796ca794e0137bb21c301efd11e1cd
Myron <dubtwee@gmail.com>
2020-07-13 10:20:36 -0500

    adding call to PUT route on form submit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,8 +3,18 @@ $(document).ready(init);
 
 function init() {
   console.log('DOM Ready for manipulation');
+  $('.js-btn-save').on('click', clickSave);
 
   getTasks();
+}
+
+function clickSave(event) {
+  const taskData = {
+    description: $('.js-field-description').val(),
+  };
+
+  saveNewTask(taskData);
+  $('.js-field-description').val('');
 }
 
 function getTasks() {
@@ -14,6 +24,21 @@ function getTasks() {
   })
     .then((response) => {
       render(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      alert('Danger Will Robinson!!!!');
+    });
+}
+
+function saveNewTask(task) {
+  $.ajax({
+    type: 'POST',
+    url: '/api/tasks',
+    data: task
+  })
+    .then((response) => {
+      getTasks();
     })
     .catch((err) => {
       console.log(err);
