e0748880d1522b0237246d9361dee88b5f57a2d4
Myron <dubtwee@gmail.com>
2020-07-13 10:00:40 -0500

    adding render after GET API call

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,33 @@ $(document).ready(init);
 
 function init() {
   console.log('DOM Ready for manipulation');
+
+  getTasks();
+}
+
+function getTasks() {
+  $.ajax({
+    type: 'GET',
+    url: '/api/tasks'
+  })
+    .then((response) => {
+      render(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      alert('Danger Will Robinson!!!!');
+    });
+}
+
+function render(tasksList) {
+  const $tasksList = $('.js-tasks-list').text('WOW');
+
+  $tasksList.empty();
+  for (let task of tasksList) {
+    $tasksList.append(`
+      <li>
+        ${task.description}, COMPLETE: ${task.complete}
+      </li>
+    `);
+  }
 }
